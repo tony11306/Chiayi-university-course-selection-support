06dc5886fd6ba63eab1c2a83e41680499612dbf7
Tony11306 <tony20020507@gmail.com>
2021-12-27 20:16:57 +0800

    更新公告

--- src/components/announcement.js
+++ src/components/announcement.js
@@ -2,8 +2,9 @@ function Announcement() {
     return (
         <div className="alert alert-success" role="alert">
             <h4 className="alert-heading">📢小公告</h4>
-            <p>【Bug】目前 ios 手機用戶點擊輸出課表的按鈕會無法正確輸出，請暫時使用電腦</p>
-            <p>【提醒】盡量善用篩選器，不然畫面元素過多會造成卡頓</p>
+            <p>【Bug】目前 ios 手機用戶點擊輸出課表的按鈕會無法正確輸出，請暫時使用電腦。</p>
+            <p>【提醒】盡量善用篩選器，不然畫面元素過多會造成卡頓。</p>
+            <p>【提醒】點擊課程清單右上角的「查看已選課程」可以移除選擇的課程。</p>
             <hr />
             <p className="mb-0">如果有什麼問題，歡迎直接寄信到 <a href='mailto:tony20020507@gmail.com'>tony11306 的信箱</a></p>
             <p className='mb-0'>或者可以到我的巴哈姆特小屋文章留言 <a href='https://home.gamer.com.tw/artwork.php?sn=5342422'>【程式作品】嘉義大學選課輔助器</a></p>
